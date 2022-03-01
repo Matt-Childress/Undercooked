dcf6df8573003fd0b98a5e61cd2ac76612c1f4c1
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-01 16:50:06 -0600

    Initial Input work
    -input manager axes defined
    -Scripts folder creation
    -ControlsManagerObject added in Main scene
    -Player 1 & 2 action buttons are now logged (v and right ctrl)

---
+++ Assets/Scripts/ControlsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("P1Action"))
+        {
+            Debug.Log("P1Action");
+        }
+
+        if (Input.GetButtonDown("P2Action"))
+        {
+            Debug.Log("P2Action");
+        }
+    }
+}
