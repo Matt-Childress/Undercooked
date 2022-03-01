540b2e41a7f876e9bbf6940b5cc44410233f4bf8
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-01 17:24:07 -0600

    movement handling added

--- Assets/Scripts/ControlsManager.cs
+++ Assets/Scripts/ControlsManager.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class ControlsManager : MonoBehaviour
 {
+    public Player player1;
+    public Player player2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this statement will handle the case that players have not been defined in the scene
+        if(!player1 || !player2)
+        {
+            var players = FindObjectsOfType<Player>();
+            foreach(var player in players)
+            {
+                if(!player1)
+                {
+                    player1 = player;
+                }
+                else if(!player2)
+                {
+                    player2 = player;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("P1Action"))
+        //listen for movement key input, and send to the appropriate player movement method
+        HandleMovement();
+        //listen for action key input, and handle appropriately
+        //HandleAction();
+    }
+
+    private void HandleMovement()
+    {
+        //getting input from keys
+        float p1H = Input.GetAxisRaw("P1Horizontal");
+        float p1V = Input.GetAxisRaw("P1Vertical");
+        float p2H = Input.GetAxisRaw("P2Horizontal");
+        float p2V = Input.GetAxisRaw("P2Vertical");
+
+        if (p1H != 0f || p1V != 0f)
         {
-            Debug.Log("P1Action");
+            player1.Move(p1H, p1V); //if horizontal or vertical keys are pressed, call the player1 movement method
         }
 
-        if (Input.GetButtonDown("P2Action"))
+        if (p2H != 0f || p2V != 0f)
         {
-            Debug.Log("P2Action");
+            player2.Move(p2H, p2V); //if horizontal or vertical keys are pressed, call the player2 movement method
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //player movement method
+    public void Move(float hIn, float vIn)
+    {
+        //make a Vector3 with horizontal along the x axis and vertical along the z axis (translated for a top-down view)
+        Vector3 direction = new Vector3(hIn, 0, vIn);
+
+        //update the player object's position with respect to the Vector3 direction and the player's speed
+        transform.position += direction * speed;
+    }
+}
