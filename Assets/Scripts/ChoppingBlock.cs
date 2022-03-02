ab2bee81c4392fd7faa4d0511ca81ce63beef2ad
Matt Childress <mattchildressgamedev@gmail.com>
2022-03-01 23:19:01 -0600

    chopping blocks placed in scene, are highlightable

---
+++ Assets/Scripts/ChoppingBlock.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoppingBlock : MonoBehaviour
+{
+    private Image blockImage;
+
+    private Color litBlockColor = Color.white;
+    private Color dimBlockColor;
+
+    private int highlightingPlayerCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //assign blockImage and unselected color on start
+        blockImage = GetComponent<Image>();
+        dimBlockColor = blockImage.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectBlock(Player player)
+    {
+        //light up the block
+        blockImage.color = Color.white;
+
+        highlightingPlayerCount++;
+
+        //replace highlighted block with the new block, dim the old block
+        if (player.highlightedChopBlock)
+        {
+            player.highlightedChopBlock.DeselectBlock(player);
+        }
+        player.highlightedChopBlock = this;
+    }
+
+    public void DeselectBlock(Player player)
+    {
+        //unassign the player's highlighted chopping block, but only if it's this block
+        if (player.highlightedChopBlock == this)
+        {
+            player.highlightedChopBlock = null;
+            highlightingPlayerCount--;
+            if (highlightingPlayerCount == 0)
+            {
+                //dim the block if this was the only player highlighting it
+                blockImage.color = dimBlockColor;
+            }
+        }
+    }
+
+    //handle when a player leaves a chopping block
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player)
+        {
+            //deselect the block
+            DeselectBlock(player);
+        }
+    }
+
+    //handle when a player contacts a chopping block, or re-highlights a block they were on the edge of before
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        Player player = collider.GetComponent<Player>();
+        if (player && !player.highlightedChopBlock)
+        {
+            //select the block
+            SelectBlock(player);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    //speed of player movement
     public float speed = 1;
 
-    //hold the player's currently highlighted vegeTable
+    //text fields above the player that display what vegetables are picked up
+    public Text heldVegetable1Text;
+    public Text heldVegetable2Text;
+
+    //hold the player's currently highlighted vegeTable and chopBlocks
     [HideInInspector]
     public VegetableTable highlightedVegetable;
+    [HideInInspector]
+    public ChoppingBlock highlightedChopBlock;
 
     //the player's held vegetables
+    [HideInInspector]
     public VegetableType heldVegetable1 = 0;
+    [HideInInspector]
     public VegetableType heldVegetable2 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //handling if heldVegetableTexts are undefined on start
+        if(!heldVegetable1Text || !heldVegetable2Text)
+        {
+            Text[] vegeTexts = GetComponentsInChildren<Text>();
+            if(!heldVegetable1Text)
+            {
+                heldVegetable1Text = vegeTexts[0];
+            }
+            if(!heldVegetable2Text)
+            {
+                heldVegetable2Text = vegeTexts[1];
+            }
+        }
     }
 
     // Update is called once per frame
@@ -62,6 +84,8 @@ public class Player : MonoBehaviour
 
     private void UpdateHeldVegetableUI()
     {
-
+        //set text field above the player to the correct held vegetables
+        heldVegetable1Text.text = heldVegetable1 != 0 ? heldVegetable1.ToString() : "";
+        heldVegetable2Text.text = heldVegetable2 != 0 ? heldVegetable2.ToString() : "";
     }
 }
